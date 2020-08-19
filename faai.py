ccdb5a4d78728baae06087c9419cec602df902e6
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-08-19 15:36:37 +0200

    Add origin param to plot_vector

--- faai.py
+++ faai.py
@@ -19,11 +19,15 @@ def canvas(xlim, ylim):
     plt.yticks(np.arange(ylim[0], ylim[1]+1))
 
 
-def plot_vector(v, color=None):
+def plot_vector(v, origin=None, color=None):
     
     v1 = v[0]
     v2 = v[1]
+
+    if origin is None:
+        origin = (0, 0)
     
     c = 'black' if color is None else color
     
-    plt.arrow(0, 0, v1, v2, head_width=.2, head_length=.4, length_includes_head=True, color=c)
+    plt.arrow(origin[0], origin[1], v1, v2, 
+              head_width=.2, head_length=.4, length_includes_head=True, color=c)
