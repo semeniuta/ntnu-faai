3745425237cf8d59eec50a84a8c29f11d971faf2
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-08-19 15:20:25 +0200

    Implement plot_vector

--- faai.py
+++ faai.py
@@ -17,3 +17,13 @@ def canvas(xlim, ylim):
 
     plt.xticks(np.arange(xlim[0], xlim[1]+1))
     plt.yticks(np.arange(ylim[0], ylim[1]+1))
+
+
+def plot_vector(v, color=None):
+    
+    v1 = v[0]
+    v2 = v[1]
+    
+    c = 'black' if color is None else color
+    
+    plt.arrow(0, 0, v1, v2, head_width=.2, head_length=.4, length_includes_head=True, color=c)
