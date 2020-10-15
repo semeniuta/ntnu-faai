ef3f9f7ff48db2225d16616738d081b85a07ec63
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-10-15 14:22:14 +0200

    Improve serial_demo.py: timestamps etc

--- communication/serial_demo.py
+++ communication/serial_demo.py
@@ -1,20 +1,28 @@
 """
-To list ports:
+To list ports (Mac/Linux):
 
 ls /dev/tty.*
 """
 
-from time import sleep
+import time
 import serial
+import datetime as dt
 
-conn = serial.Serial('/dev/tty.usbmodem1432301', 9600)
+# This has to be changed to your own device
+device_name = '/dev/tty.usbmodem143301'
+
+conn = serial.Serial(device_name, 9600)
 
 while True:
 
     try:
 
+        t_now = time.time()
+        timestamp_now = dt.datetime.fromtimestamp(t_now).strftime('%Y-%m-%d %H:%M:%S.%f')
+
         line = conn.readline()
-        print(line)
+        
+        print('[{}] {}'.format(timestamp_now, line))
 
     except KeyboardInterrupt:
         print('\nStopping the server due to keyborard interrupt')
